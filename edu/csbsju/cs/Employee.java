d06825e4ff8d96ba36eb6dfc57925cb63e06dda3
ZVETTER001 <zvetter001@vplincsci075.ad.csbsju.edu>
2018-02-21 14:42:17 -0600

    Terminal 2 in Step 5: Modified Employee.java introducing a conflict

--- edu/csbsju/cs/Employee.java
+++ edu/csbsju/cs/Employee.java
@@ -30,9 +30,9 @@ public class Employee
       return hireDay;
    }
 
-   public void raiseSalary(double byPercent)
+   public void raiseSalary(double byDecimalValue)
    {
-      double raise = salary * byPercent / 100;
+      double raise = salary * byDecimalValue;
       salary += raise;
    }
 
